75f60ffa854df7d8847aee8f096276fcf2479de6
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2023-12-13 18:39:38 +0530

    Add files via upload

--- Heap/Heap.c
+++ Heap/Heap.c
@@ -1,1 +1,54 @@
+#include <stdio.h>
 
+void insert(int H[], int n)
+{
+    int i = n, temp;
+    temp = H[i];
+
+    while (i > 1 && temp > H[i/2])
+    {
+        H[i] = H[i / 2];
+        i = i / 2;
+    }
+    H[i] = temp;
+}
+
+int delete(int H[], int n)
+{
+    int i, j, x, temp, val = H[i];
+    x = H[n];
+    H[1] = H[n];
+    i = 1;
+    j = i * 2;
+
+    while (j < n - 1)
+    {
+        if (H[j+1] > H[j])
+            j++;
+        if (H[i] < H[j])
+        {
+            temp = H[i];
+            H[i] = H[j];
+            H[j] = temp;
+            i = j;
+            j = 2 * j;
+        }
+        else
+            break;
+    }
+}
+
+void create_heap()
+{
+
+}
+
+int main(void)
+{
+    int H[] = {0, 10, 20, 30, 25, 5, 40, 35};
+
+    for (int i = 2; i <= 7; i++)
+        insert(H, i);
+
+    return 0;
+}
