4244aa53677125fffb06d47cce3036efed4a221f
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2024-01-18 12:17:19 +0530

    Update Queue.h

--- Tree/Binary_Tree/Queue.h
+++ Tree/Binary_Tree/Queue.h
@@ -1,1 +1,14 @@
+#include <malloc.h>
+#include <stdbool.h>
 
+struct qnode
+{
+    struct node *data;
+    struct qnode *next;
+};
+
+int count_nodes(struct qnode *head);
+void display(struct qnode *head);
+bool is_empty(struct qnode *head);
+void enqueue(struct qnode **head, struct node *data);
+struct node *dequeue(struct qnode **head);
